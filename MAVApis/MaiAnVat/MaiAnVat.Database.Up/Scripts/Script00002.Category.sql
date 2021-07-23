4ab31110440cbb9d5873c5f6f6ed40047e493715
Duy Nguyen Xuan <duy.nguyenxuan@ncc.asia>
2021-07-23 22:57:04 +0700

    update

--- MAVApis/MaiAnVat/MaiAnVat.Database.Up/Scripts/Script00002.Category.sql
+++ MAVApis/MaiAnVat/MaiAnVat.Database.Up/Scripts/Script00002.Category.sql
@@ -3,16 +3,15 @@
 MERGE INTO [dbo].[Category] as [Target] 
 USING (
 	VALUES
-	(N'Loại công việc',N'Danh sách loại công việc '),
-	(N'Loại hành động',N'Loại hành động'),
-	(N'Loại trạng thái',N'Loại trạng thái'),
-	(N'Loại lịch trình',N'Loại lịch trình'),
-	(N'Trạng thái công việc',N'Trạng thái công việc')
+	(N'JobType',N'Danh sách loại công việc '),
+	(N'ActionType',N'Loại hành động'),
+	(N'StatusType',N'Loại trạng thái'),
+	(N'ScheduleType',N'Loại lịch trình')
 )
 AS [Source] (
 	[Name], [Description]
 	) 
-	ON [Target].[Name] = [Source].[Name]
+	ON [Target].[Name] = [Source].[Name] OR [Target].[Description] = [Source].[Description]
 
 WHEN MATCHED THEN
 	UPDATE SET 
