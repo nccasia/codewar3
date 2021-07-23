2fac83a5359b7d8690e2315fa5764b0fdeca3c19
nguyenmanh36 <manh.nguyenvan@mission.plus>
2021-07-24 05:11:48 +0700

    update lucky member game

--- frontend/src/pages/EnglishGame/index.js
+++ frontend/src/pages/EnglishGame/index.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Slider from 'react-slick';
 import '../../assets/css/slick.css'
-
+import { Header } from '../../components/header';
+import '../style.css'
 
 let listGame = [
     {
@@ -28,97 +29,74 @@ let listGame = [
 ]
 
 export const EnglishGame = () => {
-
-    var settings = {
-        dots: true,
+    const [settings, setSetting] = React.useState({
+        dots: false,
         infinite: true,
         slidesToShow: 3,
         centerMode: true,
-        // centerPadding: "40px",
-        slidesToScroll: 1,
+        arrows: false,
+        centerPadding: "40px",
         autoplay: true,
-        speed: 1000,
+        speed: 10,
         autoplaySpeed: 0,
-        cssEase: "linear"
-    };
+    })
+
+    let slider;
+
+
+
+    const ClickPlay = () => {
+        let time = Math.floor(Math.random() * (6000 - 4000) + 4000);
+        let sp = settings.speed;
+        setInterval(() => {
+            console.log(time, sp);
+            if (time < 1000) {
+                slider?.slickPause();
+                return
+            }
+            time = time * 0.8;
+            sp = sp + 100;
+            setSetting({ ...settings, speed: sp })
+        }, time * 0.8);
+    }
+
 
     return (
-        <>
-            <div style={{ height: "100vh" }}>
-                <h2>Auto Play</h2>
-                <Slider {...settings}>
-                    <div>
-                        <Image src="https://kenh14cdn.com/thumb_w/660/203336854389633024/2021/7/9/photo-1-16257989599561090737937.jpeg" />
-                    </div>
-                    <div>
-                        <Image src="https://kenh14cdn.com/thumb_w/660/2020/4/9/photo-1-15864365280411955650737.jpg" />
-                    </div>
-                    <div>
-                        <Image src="https://i.pinimg.com/originals/7e/da/a4/7edaa44235f5c717d6d7c65c14727ee4.png" />
-                    </div>
-                    <div>
-                        <Image src="https://vcdn-giaitri.vnecdn.net/2021/02/24/j-4353-1614154422.jpg" />
-                    </div>
-                    <div>
-                        <Image src="https://static2.yan.vn/YanNews/2167221/201908/iu-la-ai-tinh-yeu-su-nghiep-bai-hat-cua-iu-4bb930f2.jpg" />
-                    </div>
-                    {/* <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>1</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>2</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>3</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>4</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>5</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>6</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>7</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>8</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>9</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>10</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>11</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>12</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>13</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>14</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>15</h3>
-                    </div>
-                    <div>
-                        <h3 style={{ color: "white", border: "1px solid white" }}>16</h3>
-                    </div> */}
+        <Div>
+            <Header />
+            <WrapperSlider>
+                <h2 style={{ marginBottom: '30px' }}>Let't find a lucky member!</h2>
+                <Slider ref={c => (slider = c)} {...settings}>
+                    <Image src="https://kenh14cdn.com/thumb_w/660/203336854389633024/2021/7/9/photo-1-16257989599561090737937.jpeg" />
+                    <Image src="https://kenh14cdn.com/thumb_w/660/2020/4/9/photo-1-15864365280411955650737.jpg" />
+                    <Image src="https://i.pinimg.com/originals/7e/da/a4/7edaa44235f5c717d6d7c65c14727ee4.png" />
+                    <Image src="https://vcdn-giaitri.vnecdn.net/2021/02/24/j-4353-1614154422.jpg" />
+                    <Image src="https://static2.yan.vn/YanNews/2167221/201908/iu-la-ai-tinh-yeu-su-nghiep-bai-hat-cua-iu-4bb930f2.jpg" />
                 </Slider>
-            </div>
-        </>
+                <Button onClick={ClickPlay}>play</Button>
+            </WrapperSlider>
+            <div className="area" >
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div >
+        </Div >
     )
 }
 
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
+
+
+const Div = styled.div`
+    height: 100vh;
 `;
 
 const Card = styled.div`
@@ -128,6 +106,16 @@ const Card = styled.div`
     color: red;
 `;
 
+const WrapperSlider = styled.div`
+    position: absolute;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: center;
+`
+
 const Image = styled.img`
     height: 200px;
     border: 1px solid white;
@@ -135,8 +123,16 @@ const Image = styled.img`
 
 const Button = styled.div`
     width: 100px;
-    height: 20px;
+    height: 40px;
+    font-size: 16px;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
     background-color: white;
     color: red;
+    margin-top: 30px;
     cursor: pointer;
+    z-index: 999;
 `;
